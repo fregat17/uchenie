68457356739b08bfb599822f26fc0b43649cfcbd
fregat17 <gavrikovalexey1@gmail.com>
2022-07-28 16:20:08 +0300

    little refactoring block_server

--- echo/block_socket.py
+++ echo/block_socket.py
@@ -1,11 +1,8 @@
 import socket
 import threading
 
-serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversock.bind(("127.0.0.1", 8888))
-serversock.listen()
-
-lock = threading.Lock()
+IP = "127.0.0.1"
+PORT = 8888
 
 
 def recv_send(connection):
@@ -14,7 +11,16 @@ def recv_send(connection):
         connection.send(data)
 
 
-while True:
-    conn, addr = serversock.accept()
-    thread = threading.Thread(target=recv_send, args=(conn,))
-    thread.start()
+def main():
+    serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversock.bind((IP, PORT))
+    serversock.listen()
+
+    while True:
+        conn, addr = serversock.accept()
+        thread = threading.Thread(target=recv_send, args=(conn,))
+        thread.start()
+
+
+if __name__ == "__main__":
+    main()
